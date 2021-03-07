bdc2c31538d17897a8294d1148a0372525db3818
Roger Ngo <urbanspr1nter@gmail.com>
2021-03-07 13:37:49 -0800

    Interfaces project to prevent Circular dependencies

---
+++ Jrpg.System/Jrpg.System.Interfaces/IContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jrpg.System.Interfaces
+{
+    public interface IContext<T>
+    {
+        void Add(string key, T value);
+        void Remove(string key);
+        T Find(string key);
+        List<T> List();
+    }
+}
